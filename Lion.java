8a3c85f40a1ab6737c6fcabb133cd0379eb8d733
Mahmut Altunkaya <55110329+Rex3FFown@users.noreply.github.com>
2024-08-06 21:51:41 +0300

    Java OOP

---
+++ Lion.java
@@ -0,0 +1,12 @@
+package zoo;
+
+public class Lion extends Animal {
+    public Lion(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Roar!");
+    }
+}
